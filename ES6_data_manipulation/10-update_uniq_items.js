ebd0db9ae203cdf5ecb12e24740f91a589fe6601
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-07-23 10:03:44 +0200

    editt the task 10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,17 +1,14 @@
 export default function updateUniqueItems(groceryMap) {
-    // Check if the argument passed is a Map
-    if (!(groceryMap instanceof Map)) {
-      throw new Error('Cannot process'); // Cannot process
-    }
-  
-    // Iterate over each entry in the map
-    groceryMap.forEach((quantity, item) => {
-      // If the quantity is 1, update the quantity to 100
-      if (quantity === 1) {
-        groceryMap.set(item, 100);
-      }
-    });
-  
-    return groceryMap; // Return the updated map
+  // Vérifie si l'argument passé est une carte
+  if (!(groceryMap instanceof Map)) {
+    throw new Error('Cannot process'); // Impossible de traiter
   }
-  
+
+  // Parcourt chaque entrée de la carte
+  groceryMap.forEach((quantity, item) => {
+    // Si la quantité est de 1, met à jour la quantité à 100
+    if (quantity === 1) { groceryMap.set(item, 100); }
+  });
+
+  return groceryMap; // Retourne la carte mise à jour
+}
