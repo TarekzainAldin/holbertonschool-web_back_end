5599402f30da44c9969edf0414992e57272eac7b
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-08-16 13:08:31 +0200

    try another code

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
-"""contient la fonction measure_time"""
+"""
+task coming .
+"""
+
 import asyncio
+import time
+
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n, max_delay):
-    """retourne le temps d'execution de la fonction wait_n
-    qui est le temps d'execution de la coroutine la plus longue"""
-    delais = asyncio.run(wait_n(n, max_delay))
-    return (max(delais))
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measures the total execution time for wait_n(n, max_delay),
+    and returns total_time / n.
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    return total_time / n
