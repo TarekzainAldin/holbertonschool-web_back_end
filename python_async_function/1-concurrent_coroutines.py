db237a058b4221c670c98d595103c982c9035283
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-08-16 12:49:59 +0200

    change the code

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python3
-""""contient méthode asynchrone wait_n"""
+""" Concurrently run multiple instances of the wait_random coroutine. """
 
 import asyncio
+from typing import List
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
-    """lance n tâches asynchrones 'wait_random' qui s'exécutent en parallèle
-    et dure au maximum max_delay secondes"""
-
-    liste_operations = []
-    # Boucle pour créer une liste de n operations asynchrones
-    for i in range(n):
-        liste_operations.append(wait_random(max_delay))
-
-    # lance et attend que toutes les opérations de la liste soient terminées
-    '''liste_delais = await asyncio.gather(*liste_operations)
-
-    return sorted(liste_delais)'''
-
-    # autre solution
-    liste_delais = []
-    for operation in asyncio.as_completed(liste_operations):
-        delais = await operation
-        liste_delais.append(delais)
-
-    return liste_delais
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """delay_n = await asyncio.gather(
+        *[wait_random(max_delay) for _ in range(n)])"""
+    list_of_delay = []
+    for turn in range(n):
+        list_of_delay.append(await wait_random(max_delay))
+    return sorted(list_of_delay)
