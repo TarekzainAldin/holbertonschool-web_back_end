303eb0820e08098d8218c17d22d72cfd9b10ac6a
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-08-16 12:31:35 +0200

    edit the style

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,14 +1,11 @@
- #!/usr/bin/env python3
-"""c
-ontient méthode
- asynchrone 
- wait_random"""
+#!/usr/bin/env python3
+"""contient méthode asynchrone wait_random"""
 import random
 import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Simulation opération asynchrone de 0"""
+    """Simulation opération asynchrone de 0 à max seconds aléatoirement"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
