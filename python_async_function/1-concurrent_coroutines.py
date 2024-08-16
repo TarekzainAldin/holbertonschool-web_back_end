f07d91e0a11b24334ba3bd95e73df57bd56c9273
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-08-16 13:17:15 +0200

    try the another code

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-""" Concurrently run multiple instances of the wait_random coroutine. """
+"""
+task.
+"""
 
 import asyncio
 from typing import List
-
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """delay_n = await asyncio.gather(
-        *[wait_random(max_delay) for _ in range(n)])"""
-    list_of_delay = []
-    for turn in range(n):
-        list_of_delay.append(await wait_random(max_delay))
-    return sorted(list_of_delay)
+    """
+    Spawn wait_random n times with the specified max_delay.
+    Return the list of delays in ascending order.
+    """
+    delay = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
+    return sorted(delay)
