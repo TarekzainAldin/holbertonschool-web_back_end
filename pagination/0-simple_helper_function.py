f465a00474b661658ef979eda89b68cdc72b5af7
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-08-20 13:54:09 +0200

    edit the code

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
-"""contatnte the function index"""
+#!/usr/bin/env python3
+"""Contient la fonction index_range."""
 
 
 def index_range(page, page_size):
-    """"return tuble for the tow args """
+    """Retournes un tuple de deux éléments correspondant à l'index de départ
+    et l'index de fin pour une page donnée."""
 
     start = (page - 1) * page_size
     end = start + page_size
