e53ea6495625ae9d92a372496e1922badd42b54d
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-08-23 11:06:23 +0200

    fix the code

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-"""contient la méthode measure_runtime"""
-import time
+
+""" Let's execute multiple coroutines at the same time with async """
+
 import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Execute 4 fois async_comprehension de maniere asynchrone
-    et mesure le temps d'exécution total. """
-    tic = time.time()
+    """ Measure  tarek """
+    start = asyncio.get_event_loop().time()
     await asyncio.gather(*[async_comprehension() for _ in range(4)])
-    tac = time.time()
-    return (tac - tic)
-    # On obtient un temps d'exécution d'environ 10 secondes car les 4 appels
-    # sont exécutés en parallèle et chacun prend environ 10 x 1 secondes.
+    end = asyncio.get_event_loop().time()
+    return end - start
