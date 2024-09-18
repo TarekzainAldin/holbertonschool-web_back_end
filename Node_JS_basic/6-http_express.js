294b771c68cd32d255577262c00640cd1f2f745f
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-09-18 10:34:48 +0200

    edit the space in the file

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,24 +1,20 @@
 /* eslint-disable no-undef */
-//import the experss moudle 
-const experss = require('express');
+// Import the express module
+const express = require('express');
 
+// Initialize the Express application
+const app = express();
 
-// intialize the Experss application 
-const app = experss();
-
-
-//Define a reout for the root URL 
-
-app.get ('/',(req, res)=> {
-    res.send('Hello Holberton Shcool');
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!'); // Send the response
 });
 
-
-//make the app listen on port 12345
-const port = 12345;
-app.listen (port,() => {
-    console.log('server is runing on port ${port}');
+// Make the app listen on port 1245
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
-// export the app for use in other modules 
-module.exports = app ;
+// Export the app for use in other modules
+module.exports = app;
