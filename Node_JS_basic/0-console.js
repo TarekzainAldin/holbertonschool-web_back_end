b248b7a687a11a8cfe6c0f99f72e83c9878fd325
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-09-18 08:25:46 +0200

    ading the commint

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,11 @@
+/**
+ * Prints a string to STDOUT.
+ * @param {string} message - The message to be printed.
+ */
 function displayMessage(message) {
     console.log(message);
   }
   
+  // Export the function
   module.exports = displayMessage;
   
