bcfdd70d2b67f23b81dc05acd2096c275721312f
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-09-18 09:29:10 +0200

    chang the test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,23 +1,13 @@
+#!/usr/bin/env node
 /* eslint-disable no-undef */
-// Display the initial message
+
 console.log('Welcome to Holberton School, what is your name?');
-
-// Enable stdin to read user input
-process.stdin.setEncoding('utf8');
-
-// Listen for user input
 process.stdin.on('readable', () => {
-    const input = process.stdin.read();
-    if (input !== null) {
-        const name = input.trim(); // Trim any extra spaces or newlines
-        console.log(`Your name is: ${name}`);
-
-        // Once the name is displayed, close the input stream
-        process.stdin.end();
-    }
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
-
-// When stdin ends, display the exit message
 process.stdin.on('end', () => {
-    console.log('This important software is now closing');
+  console.log('This important software is now closing');
 });
