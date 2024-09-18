b7b4cb51faedb16c4b1f791d03ee0e602bf5fc9a
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-09-18 10:47:00 +0200

    testing another tesst

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,65 +1,61 @@
-/* eslint-disable no-unused-vars */
+#!/usr/bin/env node
 /* eslint-disable no-undef */
 const express = require('express');
 const fs = require('fs');
-
-// Initialize the Express application
 const app = express();
+const PORT = 1245;
 
-// Function to read students from the database
-const countStudents = (path) => {
-  try {
-    // Read the CSV file synchronously
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n').filter(line => line.length > 0);
-
-    if (lines.length <= 1) {
-      return 'Cannot load the database';
-    }
-
-    const studentsByField = {};
-
-    for (let i = 1; i < lines.length; i++) {
-      const [firstname, lastname, age, field] = lines[i].split(',');
-      if (firstname && field) {
-        if (!studentsByField[field]) {
-          studentsByField[field] = [];
-        }
-        studentsByField[field].push(firstname);
+// Function to read and count students from a CSV file asynchronously
+const countStudentsFromCSV = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        return reject(new Error('Cannot load the database'));
       }
-    }
-
-    const totalStudents = Object.values(studentsByField).reduce((acc, curr) => acc + curr.length, 0);
-    let response = `Number of students: ${totalStudents}\n`;
-
-    for (const field in studentsByField) {
-      const studentNames = studentsByField[field].join(', ');
-      response += `Number of students in ${field}: ${studentsByField[field].length}. List: ${studentNames}\n`;
-    }
-
-    return response.trim(); // Trim to remove extra newline at the end
-  } catch (error) {
-    return 'Cannot load the database';
-  }
+      const lines = data.trim().split('\n').filter(line => line.length > 0);
+      if (lines.length <= 1) {
+        return reject(new Error('Cannot load the database'));
+      }
+      const studentsByField = {};
+      for (let i = 1; i < lines.length; i++) {
+        // eslint-disable-next-line no-unused-vars
+        const [firstname, lastname, age, field] = lines[i].split(',').map(item => item.trim());
+        if (firstname && field) {
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+          studentsByField[field].push(firstname);
+        }
+      }
+      const totalStudents = Object.values(studentsByField).reduce((acc, curr) => acc + curr.length, 0);
+      const response = [`Number of students: ${totalStudents}`];
+      for (const field in studentsByField) {
+        response.push(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+      }
+      resolve(response.join('\n'));
+    });
+  });
 };
 
-// Define a route for the root URL
+// Root endpoint
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!'); // Send the response for the root path
+  res.send('Hello Holberton School!');
 });
 
-// Define a route for /students
-app.get('/students', (req, res) => {
-  const dbPath = 'path/to/your/database.csv'; // Update this path to your CSV file
-  const studentList = countStudents(dbPath);
-  res.send(`This is the list of our students\n${studentList}`);
+// Students endpoint
+app.get('/students', async (req, res) => {
+  try {
+    const result = await countStudentsFromCSV(process.argv[2]);
+    res.send(`This is the list of our students\n${result}`);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-// Make the app listen on port 1245
-const PORT = 1245;
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// Export the app for use in other modules
+// Export the app variable
 module.exports = app;
