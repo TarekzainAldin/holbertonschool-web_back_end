606ce2ee1cef63a19e7faba5768efbc4b7ddb7d8
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-09-18 09:17:10 +0200

    edit the code

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,13 +1,7 @@
-/**
- * Prints a string to STDOUT.
- * @param {string} message - The message to be printed.
- */
-
-
 function displayMessage(message) {
     console.log(message);
   }
   
   // Export the function
-  module.exports = displayMessage;
+  export default displayMessage;
   
