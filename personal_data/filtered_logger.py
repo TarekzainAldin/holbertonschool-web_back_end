b8d982abb0f1dfc6f219a4643a82b81ae6c4259f
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-13 12:27:41 +0100

    1. Log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -30,24 +30,31 @@ def filter_datum(
         lambda m: f"{m.group(1)}={redaction}",
         message)
 
+
 class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
     """
-    Redacting Formatter class 
-    """
-    REDACTION="***"
+    REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR=";"
-    
+    SEPARATOR = ";"
 
-    def __init__(self,fields:List[str]):
-        """  Initialize RedactingFormatter with fields to redact."""
+    def __init__(self, fields: List[str]):
+        """
+        Initialize RedactingFormatter with fields to redact.
+        """
         super().__init__(self.FORMAT)
-        self.fields=fields
+        self.fields = fields
 
-    def format(self, record:logging.LogRecord)->str:
-        """ Format log record, redacting specified fields."""
-        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format log record, redacting specified fields.
+        """
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR
+        )
         return super().format(record)
+
+
 if __name__ == "__main__":
     import sys
     print(filter_datum(["password"], "***", "user=admin;password=secret", ";"))
