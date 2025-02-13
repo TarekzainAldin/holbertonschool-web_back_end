ee86db1d3726585880eaa87767c25207330d6971
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-13 12:15:43 +0100

    1. Log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,7 +2,7 @@
 """
 Module for filtering and obfuscating log messages.
 """
-
+import logging
 import re
 from typing import List
 
@@ -30,7 +30,24 @@ def filter_datum(
         lambda m: f"{m.group(1)}={redaction}",
         message)
 
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class 
+    """
+    REDACTION="***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR=";"
+    
 
+    def __init__(self,fields:List[str]):
+        """  Initialize RedactingFormatter with fields to redact."""
+        super().__init__(self.FORMAT)
+        self.fields=fields
+
+    def format(self, record:logging.LogRecord)->str:
+        """ Format log record, redacting specified fields."""
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
 if __name__ == "__main__":
     import sys
     print(filter_datum(["password"], "***", "user=admin;password=secret", ";"))
