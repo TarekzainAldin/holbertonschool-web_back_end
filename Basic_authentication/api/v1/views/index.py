860e6722c77341279d6f79ac963941ceecfdcf3f
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-16 14:31:51 +0100

    1. Error handler: Unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint():
+    """ Endpoint that raises a 401 Unauthorized error """
+    abort(401)
