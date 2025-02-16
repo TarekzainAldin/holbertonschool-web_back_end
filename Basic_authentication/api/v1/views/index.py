e17282a4c02c98d737b359882748edc7d5945dfb
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-16 14:39:25 +0100

    py

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -25,6 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized_endpoint():
     """ Endpoint that raises a 401 Unauthorized error """
