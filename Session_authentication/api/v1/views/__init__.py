843da34bb609bade04e280b2cdd0fa5e0337bbb9
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-17 16:21:08 +0100

    fix init

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""" DocDocDocDocDocDoc
-"""
+""" Initialize Blueprint for views """
 from flask import Blueprint
 
+# Define the blueprint once
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+# Import all route modules AFTER defining the blueprint
 from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.session_auth import *
