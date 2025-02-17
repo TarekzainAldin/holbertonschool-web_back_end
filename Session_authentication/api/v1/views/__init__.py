428093b92ecc51dea39519ef2a08d015702cf667
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-17 16:38:11 +0100

    edit the inite file

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python3
-""" DocDocDocDocDocDoc
-"""
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+# Import routes correctly
 from api.v1.views.index import *
 from api.v1.views.users import *
-from api.v1.views.session_auth import *
+from api.v1.views.session_auth import auth_session_login, auth_session_logout  # ✅ Import only the functions
+
 
 User.load_from_file()
