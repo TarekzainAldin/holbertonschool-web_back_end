fc048ef04b6c8e31119405d0349a37ac69761e79
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-17 13:05:33 +0100

    try nth

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
-""" Module of authentification """
-from typing import List
+"""
+Class to manage the API authentication
+"""
+
 from flask import request
+from typing import List, TypeVar
+
+User = TypeVar('User')
 
 
-class Auth:
-    """ Authentification class """
+class Auth():
+    """ Class to manage the API authentication
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Checks if authentication is required for the given path """
-        if not path:
+        """ function require_auth
+        """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
-
-        if not excluded_paths or len(excluded_paths) == 0:
-            return True
-
-        # Check if path is in excluded_paths
         for excluded_path in excluded_paths:
-            # Normalize paths to be slash tolerant
-            if path.rstrip('/') == excluded_path.rstrip('/'):
+            if excluded_path.startswith(path):
                 return False
-
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Returns the Authorization header value from the request """
-        if not request:
+        """ function authorization_header
+        """
+        if request is None:
             return None
-        if 'Authorization' not in request.headers:
+        if "Authorization" not in request.headers:
             return None
-        return request.headers['Authorization']
+        return request.headers["Authorization"]
 
-    def current_user(self, request=None) -> str:
-        """ Returns None """
+    def current_user(self, request=None) -> User:
+        """ function current_user
+        """
         return None
