9a4068268407168bf44ad199d2fc01a047e06945
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-24 17:07:56 +0100

    make pycode

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,9 +6,8 @@ from bcrypt import hashpw, gensalt
 from sqlalchemy.orm.exc import NoResultFound
 
 
-
 def _hash_password(password: str) -> bytes:
     """Hash a password with bcrypt and return the hashed password as bytes."""
     salt = bcrypt.gensalt()  # Generate a salt
-    hashed_password = bcrypt.hashpw(password.encode(), salt)  # Hash the password
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
