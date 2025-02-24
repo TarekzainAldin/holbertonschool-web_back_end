78a59331b187f9d41860525cdc337a0681e4f888
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-24 18:12:40 +0100

    make py code style

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,11 +2,12 @@
 """Route model for the API"""
 
 from flask import Flask, jsonify, request, abort, redirect, url_for
-from sqlalchemy.orm.exc import NoResultFound 
+from sqlalchemy.orm.exc import NoResultFound
 
 from auth import Auth
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
@@ -17,5 +18,5 @@ def index() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
-if __name__ == '__main__': 
-    app.run(host="0.0.0.0", port=5000) 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
