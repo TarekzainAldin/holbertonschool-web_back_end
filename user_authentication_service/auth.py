448dfbf28733bf667b42dded1b1cd23dcc743125
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-24 17:10:36 +0100

    try

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,10 +4,16 @@ from user import User
 import bcrypt
 from bcrypt import hashpw, gensalt
 from sqlalchemy.orm.exc import NoResultFound
-
+from uuid import uuid4
 
 def _hash_password(password: str) -> bytes:
     """Hash a password with bcrypt and return the hashed password as bytes."""
     salt = bcrypt.gensalt()  # Generate a salt
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
+
+def _generate_uuid() -> str:
+    """ Generates UUID
+    Returns string representation of new UUID
+    """
+    return str(uuid4())
