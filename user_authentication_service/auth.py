64669d75a65cd7a27516649db6adab9f7a130767
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-24 17:51:47 +0100

    register

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -18,3 +18,23 @@ def _hash_password(password: str) -> bytes:
 def _generate_uuid() -> str:
     """Generate a string representation of a new UUID."""
     return str(uuid.uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user if the email does not exist,
+            else raise ValueError.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
