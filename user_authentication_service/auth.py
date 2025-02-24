b7f7192d09d7ba85e01e44d2927afe92051b4a0d
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-24 16:59:29 +0100

    4. Hash password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
-from user import user
 from db import DB
-
+from user import User
 
 from bcrypt import hashpw, gensalt, checkpw
-from sqlalchemy.orm.exc import NotResulteFound
+from sqlalchemy.orm.exc import NoResultFound
 
 
 
 def _hash_password(password: str) -> str:
-  """ Takes in string arg, converts to unicode
+    """ Takes in string arg, converts to unicode
     Returns salted, hashed pswd as bytestring
     """
-  return hashpw(password.encode('utf-8'), gensalt())
+    return hashpw(password.encode('utf-8'), gensalt())
