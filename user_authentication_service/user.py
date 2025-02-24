abe6029c03bc71032c4cffbe822ca9611d242c04
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-24 11:44:57 +0100

    try withuot the __reper__ function

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,19 +1,17 @@
-#!/usr/bin/python3
-
-from sqlalchemy import Column, Integer, String
+#!/usr/bin/env python3
+""" SQLAlchemy model for User
+"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class User(Base):
-    __tablename__ = "users"
-
+    """ User class """
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False, unique=True)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True) 
-    reset_token = Column(String(250), nullable=True)  
-
-    def __repr__(self):
-        return f"<User(email='{self.email}', hashed_password='{self.hashed_password}', session_id='{self.session_id}')>"
-
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
