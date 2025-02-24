bbff3c7be49bca944f482cd01c888abb9fb11e3b
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-24 18:07:37 +0100

    jsonify({"message": "Bienvenue"})

---
+++ user_authentication_service/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Route model for the API"""
+
+from flask import Flask, jsonify, request, abort, redirect, url_for
+from sqlalchemy.orm.exc import NoResultFound 
+
+from auth import Auth
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+    Return:
+      - JSON payload
+    """
+    return jsonify({"message": "Bienvenue"}),200
+
+if __name__ == '__main__': 
+    app.run(host="0.0.0.0", port=5000) 
