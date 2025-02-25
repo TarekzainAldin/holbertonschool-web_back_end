08271305e888dc34992342809e200a328749e0c4
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-25 10:41:43 +0100

    registering user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -19,5 +19,36 @@ def index() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ POST /users
+    Registers new user with email and pswd in x-www-form-urlencoded request,
+    or finds if user already registered based on email
+    Return:
+      - JSON payload
+    """
+
+    """ form-data uses request.form, body JSON uses request.get_json() """
+    form_data = request.form
+
+    if "email" not in form_data:
+        return jsonify({"message": "email required"}), 400
+    elif "password" not in form_data:
+        return jsonify({"message": "password required"}), 400
+    else:
+
+        email = request.form.get("email")
+        pswd = request.form.get("password")
+
+        try:
+            new_user = AUTH.register_user(email, pswd)
+            return jsonify({
+                "email": new_user.email,
+                "message": "user created"
+            })
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
