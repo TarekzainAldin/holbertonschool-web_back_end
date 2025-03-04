57038edf9c6167b30655f3e7a8b4ef5b1b708ee3
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-03-04 17:15:04 +0100

    try

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
+"""api flask basic """
 
-
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,8 +9,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """render template for index html """
-    return render_template('templates/index.html', title='index page')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
