a7fb2958707f11373182228f0d4ab2b475bdb050
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-03-19 16:20:04 +0100

    get and get string and get init

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,6 +1,12 @@
-import redis
+#!/usr/bin/env python3
+"""
+    String Redis
+"""
+from uuid import uuid4
+from typing import Union, Callable
+from functools import wraps
 import uuid
-from typing import Union
+import redis
 
 class Cache:
     def __init__(self):
@@ -13,3 +19,35 @@ class Cache:
         key = str(uuid.uuid4())  # Generate a random UUID as a string
         self._redis.set(key, data)  # Store the data in Redis
         return key
+    
+    def get(self, key: str, fn: Callable = None)\
+            -> Union[str, bytes, int, float]:
+        """
+            Store the cache
+
+            Args:
+                data: bring the information to store
+
+            Return:
+                Key or number uuid
+        """
+        key = self._redit.get(key)
+
+        if fn:
+            return fn(key)
+
+        return key
+
+    def get_str(self, key: str) -> str:
+        """ Parametrized get str """
+        return self._redit.get(key).decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ Parametrized get int """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+
+        return value
