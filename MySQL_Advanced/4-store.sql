131e36799b96db169b18d9de4630e8a2c79cd8f3
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-03-21 12:23:04 +0100

    adding stor file

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,15 +1,12 @@
--- Initial
-DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS orders;
+-- script that creates a trigger that decreases the quantity of an item after adding a new order
+DELIMITER //
+CREATE TRIGGER decrease
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+	UPDATE items
+	SET quantity = quantity - NEW.number
+	WHERE items.name = NEW.item_name;
+END; //
 
-CREATE TABLE IF NOT EXISTS items (
-    name VARCHAR(255) NOT NULL,
-    quantity int NOT NULL DEFAULT 10
-);
-
-CREATE TABLE IF NOT EXISTS orders (
-    item_name VARCHAR(255) NOT NULL,
-    number int NOT NULL
-);
-
-INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
+DELIMITER ;
