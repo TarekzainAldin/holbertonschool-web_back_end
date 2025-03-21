f84efc643ece2d17d365e58e27bdcdb2744c7a5b
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-03-21 12:25:29 +0100

    triger

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,20 +1,18 @@
--- 7-compute_average_score.sql
-
-DELIMITER $$
-
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+-- script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN p_user_id INT
+    )
 BEGIN
-    DECLARE avg_score DECIMAL(10,2);
+	DECLARE avg_score DECIMAL(10, 2);
 
-    -- Compute the average score for the given user
-    SELECT AVG(score) INTO avg_score 
-    FROM corrections 
-    WHERE user_id = ComputeAverageScoreForUser.user_id;
+    SELECT AVG(score) INTO avg_score
+ 	FROM corrections
+ 	WHERE user_id = p_user_id;
 
-    -- Update the users table with the computed average score
-    UPDATE users 
-    SET average_score = avg_score 
-    WHERE id = user_id;
-END$$
-
+	UPDATE users
+    SET average_score = avg_score
+    WHERE id = p_user_id;
+END //
 DELIMITER ;
