d37d976c0646b57b86db9f6b93ecb58a5aedbe37
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-03-27 18:10:34 +0100

    ok replay  value 100   for the school holberton and

---
+++ queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,14 @@
+import redis from 'redis';
+
+// Create Redis client
+const client = redis.createClient();
+
+// Handle connection events
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
